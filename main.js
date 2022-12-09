8df343c7d3452d93b5023a8410be95c6e26554cd
LeonardoRafaelDev <leonardorafaeldev@gmail.com>
2022-12-08 22:46:30 -0300

    friction ok

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var scene = document.getElementById("scene");
 
 var parallaxInstance = new Parallax(scene, {
   relativeInput: true,
-  scalarX: 100, // regular a velocidade e distancia do movimento
+  scalarX: 10, // regular a velocidade e distancia do movimento
   scalarY: 10, // regular a velocidade e distancia do movimento
-  //   frictionX: 0.1, // velocidade do movimento
-  //   frictionY: 0.1, // velocidade do movimento
+  frictionX: 2, // regula a velocidade da fricção de seguir o mouse ou seja regula a velocidade que elas andam para suas futuras posições
+  frictionY: 2, // regula a velocidade da fricção de seguir o mouse ou seja regula a velocidade que elas andam para suas futuras posições
   //   originX: 0.0, // posição inicial
   //   originY: 0.0, // posição inicial
   //   limitX: false,

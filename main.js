66fc4ffc85d893763be8316b0b8975b890bf316b
LeonardoRafaelDev <leonardorafaeldev@gmail.com>
2022-12-08 22:41:08 -0300

    scalar ok

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ var scene = document.getElementById("scene");
 
 var parallaxInstance = new Parallax(scene, {
   relativeInput: true,
-  scalarX: 40, // velocidade do movimento
-  scalarY: 40, // velocidade do movimento
-  frictionX: 0.1, // velocidade do movimento
-  frictionY: 0.1, // velocidade do movimento
-  originX: 0.0, // posição inicial
-  originY: 0.0, // posição inicial
-  limitX: false,
-  limitY: false,
-  scalar: 1,
-  friction: 0.2,
-  origin: { x: 0.5, y: 0.5 },
+  scalarX: 100, // regular a velocidade e distancia do movimento
+  scalarY: 10, // regular a velocidade e distancia do movimento
+  //   frictionX: 0.1, // velocidade do movimento
+  //   frictionY: 0.1, // velocidade do movimento
+  //   originX: 0.0, // posição inicial
+  //   originY: 0.0, // posição inicial
+  //   limitX: false,
+  //   limitY: false,
+  //   scalar: 1,
+  //   friction: 0.2,
+  //   origin: { x: 0.5, y: 0.5 },
 });
